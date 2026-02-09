38288de1013ff52499e37756027f46d71ad3ecb4
Manh Nguyen <manhnb1911@gmail.com>
2026-02-09 11:15:26 +0700

    1.1.6

--- Scripts/ExternalAssets/IExternalAssetsManager.cs
+++ Scripts/ExternalAssets/IExternalAssetsManager.cs
@@ -24,6 +24,23 @@ namespace UniT.ResourceManagement
         public UniTask<AudioClip> DownloadAudioClipAsync(string url, AudioType audioType, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default);
 
         public UniTask DownloadFileAsync(string url, string savePath, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default);
+
+        #region Uri
+
+        public UniTask<string> DownloadTextAsync(Uri uri, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadTextAsync(uri.ToString(), cache, progress, cancellationToken);
+
+        public UniTask<byte[]> DownloadBufferAsync(Uri uri, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadBufferAsync(uri.ToString(), cache, progress, cancellationToken);
+
+        public UniTask<Texture2D> DownloadTextureAsync(Uri uri, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadTextureAsync(uri.ToString(), cache, progress, cancellationToken);
+
+        public UniTask<Sprite> DownloadSpriteAsync(Uri uri, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadSpriteAsync(uri.ToString(), cache, progress, cancellationToken);
+
+        public UniTask<AudioClip> DownloadAudioClipAsync(Uri uri, AudioType audioType, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadAudioClipAsync(uri.ToString(), audioType, cache, progress, cancellationToken);
+
+        public UniTask DownloadFileAsync(Uri uri, string savePath, bool cache = true, IProgress<float>? progress = null, CancellationToken cancellationToken = default) => this.DownloadFileAsync(uri.ToString(), savePath, cache, progress, cancellationToken);
+
+        #endregion
+
         #else
         public IEnumerator DownloadTextAsync(string url, Action<string> callback, bool cache = true, IProgress<float>? progress = null);
 
@@ -36,6 +53,23 @@ namespace UniT.ResourceManagement
         public IEnumerator DownloadAudioClipAsync(string url, AudioType audioType, Action<AudioClip> callback, bool cache = true, IProgress<float>? progress = null);
 
         public IEnumerator DownloadFileAsync(string url, string savePath, Action? callback = null, bool cache = true, IProgress<float>? progress = null);
+
+        #region Uri
+
+        public IEnumerator DownloadTextAsync(Uri uri, Action<string> callback, bool cache = true, IProgress<float>? progress = null) => this.DownloadTextAsync(uri.ToString(), callback, cache, progress);
+
+        public IEnumerator DownloadBufferAsync(Uri uri, Action<byte[]> callback, bool cache = true, IProgress<float>? progress = null) => this.DownloadBufferAsync(uri.ToString(), callback, cache, progress);
+
+        public IEnumerator DownloadTextureAsync(Uri uri, Action<Texture2D> callback, bool cache = true, IProgress<float>? progress = null) => this.DownloadTextureAsync(uri.ToString(), callback, cache, progress);
+
+        public IEnumerator DownloadSpriteAsync(Uri uri, Action<Sprite> callback, bool cache = true, IProgress<float>? progress = null) => this.DownloadSpriteAsync(uri.ToString(), callback, cache, progress);
+
+        public IEnumerator DownloadAudioClipAsync(Uri uri, AudioType audioType, Action<AudioClip> callback, bool cache = true, IProgress<float>? progress = null) => this.DownloadAudioClipAsync(uri.ToString(), audioType, callback, cache, progress);
+
+        public IEnumerator DownloadFileAsync(Uri uri, string savePath, Action? callback = null, bool cache = true, IProgress<float>? progress = null) => this.DownloadFileAsync(uri.ToString(), savePath, callback, cache, progress);
+
+        #endregion
+
         #endif
 
         public void DeleteCache(string key);
